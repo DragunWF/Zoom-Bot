ee8b33b17e812c1fcf3f3a9a44f7be077113826e
DragonWF <plarisanmarc@gmail.com>
2022-04-25 01:45:52 +0800

    Implemented startup greeting function

--- main.py
+++ main.py
@@ -2,11 +2,18 @@ from datetime import datetime
 from time import sleep
 from classes.automation import AutomationExecutor
 from classes.time_checker import TimeChecker
+from classes.utils import Utils
 
 
 def startup_greeting():
-    date = datetime.now().split(" ")[0]
+    year = TimeChecker.get_year()
+    month = TimeChecker.get_month()
+    day_of_month = TimeChecker.get_day_of_month()
+    day_of_week = TimeChecker.get_day()
     hour = TimeChecker.format_hour(TimeChecker.get_hour())
+
+    Utils.colored_print(f"{month} {day_of_month} {year} | {hour} {day_of_week}",
+                        color="cyan")
 
 
 def main():
