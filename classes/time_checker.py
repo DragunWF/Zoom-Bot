40f8f56fdf562e35a46648da5ec9a3429861a7d4
DragonWF <plarisanmarc@gmail.com>
2022-04-25 00:34:19 +0800

    Implemented hour formatting and month extraction

--- classes/time_checker.py
+++ classes/time_checker.py
@@ -3,16 +3,27 @@ from datetime import datetime
 
 class TimeChecker:
     @staticmethod
-    def check_time():
-        pass
+    def format_hour(hour: str) -> str:
+        hour_value = int(hour.split(":")[0])
+        hour_of_day = hour_value - 12 if hour_value >= 13 else hour_value
+        formatted_hour = f'{hour_of_day}:{hour.split(":")[1]}'
+        mid_day = "PM" if hour_value >= 12 else "AM"
+        return f"{formatted_hour} {mid_day}"
 
     @staticmethod
-    def check_hour() -> str:
+    def extract_month(date: str) -> str:
+        months = ("january", "february", "march",
+                  "april", "may", "june", "september",
+                  "october", "november", "december")
+        return months[int(date.split("-")[1])].capitalize()
+
+    @staticmethod
+    def get_hour() -> str:
         now = datetime.now()
         return str(now).split()[1].split(".")[0][0:5]
 
     @staticmethod
-    def check_day() -> str:
+    def get_day() -> str:
         days_of_week = ("sunday", "monday", "tuesday", "wednesday",
                         "thursday", "friday", "saturday")
         start, now = datetime(2022, 1, 1), datetime.now()
