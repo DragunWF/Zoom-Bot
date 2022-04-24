234bcda01c205cfa2b7a5dfe10fbc145e065a97f
DragonWF <plarisanmarc@gmail.com>
2022-04-24 23:46:25 +0800

    Implemented get_path() method

--- classes/utils.py
+++ classes/utils.py
@@ -1,3 +1,4 @@
+import os
 import pyttsx3
 from colored import fg
 
@@ -8,16 +9,25 @@ voice_engine.setProperty("voice", voices[0].id)
 
 class Utils:
     @staticmethod
-    def text_to_speech(text : str):
+    def text_to_speech(text: str):
         voice_engine.say(text)
         voice_engine.runAndWait()
 
     @staticmethod
-    def colored_print(text : str, color : str):
+    def colored_print(text: str, color: str):
         formatted_color = fg(f"light_{color}") if color != "white" else fg("white")
         print(formatted_color + text)
 
     @staticmethod
-    def tts_print(text : str, color : str):
+    def tts_print(text: str, color: str):
         Utils.text_to_speech(text)
         Utils.colored_print(text, color)
+    
+    @staticmethod
+    def get_path() -> str:
+        root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+        while "\\" in root:
+            root = root.replace("\\", "/")
+        root = "".join([i if i != ":" else f"{i}/" for i in root])
+        
+        return f"{root[0].upper()}{root[1:]}"
