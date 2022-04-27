456c9a20d2d82f00c6999d56d8b59601c4982cc3
DragonWF <plarisanmarc@gmail.com>
2022-04-27 20:09:47 +0800

    Renewed meeting name validation

--- classes/db_tool.py
+++ classes/db_tool.py
@@ -9,36 +9,23 @@ class DatabaseTool:
         root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
         self.__path = f"{root}\\sql\\db.sqlite3"
         self.__meeting_names = []
+        self.__fill_meeting_names()
 
+    def __fill_meeting_names(self):
         with sqlite3.connect(self.__path) as db:
-            names = db.execute("SELECT name FROM meetings").fetchall()
-            self.__meeting_names = [i for i in names]
-
-    def __check_if_meeting_exists(self, meeting_name: str) -> bool:
-        with sqlite3.connect(self.__path) as db:
-            query = f"SELECT name FROM meetings WHERE meeting_name = {meeting_name}"
-            return bool(db.execute(query).fetchall())
+            data = db.execute("SELECT name FROM meetings").fetchall()
+            self.__meeting_names = [i[0] for i in data]
 
     def insert_meeting_with_validation(self, meeting_name: str):
         name = "_".join(meeting_name.lower().split(" "))
-        if not DatabaseTool.__check_if_meeting_exists(name):
+        if not name in self.__meeting_names:
             with sqlite3.connect(self.__path) as db:
                 db.execute(f"INSERT INTO meetings (name) VALUES ({name})")
                 db.commit()
+        self.__fill_meeting_names()
 
     def insert_meeting_join_record(self):
         pass
 
     def insert_program_open_record(self):
         pass
-
-    def insert_record(self, record_type: str):
-        tables = {"meeting_joined": "meeting_joins",
-                  "program_opened": "times_program_opened"}
-        date = Utils.get_date_string()
-        hour = TimeChecker.format_hour(TimeChecker.get_hour())
-
-        with sqlite3.connect(self.__path) as db:
-            db.execute(
-                f"INSERT INTO {tables[record_type]} (date, hour) VALUES ({date} {hour})")
-            db.commit()
