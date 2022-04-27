61486859eb27181c05efa1f75522d4c04dd05e61
DragonWF <plarisanmarc@gmail.com>
2022-04-27 20:39:39 +0800

    Implemented main logic for inserting and validating

--- classes/db_tool.py
+++ classes/db_tool.py
@@ -11,21 +11,39 @@ class DatabaseTool:
         self.__meeting_names = []
         self.__fill_meeting_names()
 
+    def __format_meeting_name(self, name: str) -> str:
+        return "_".join(name.lower().split(" "))
+
+    def __get_date_and_hour(self):
+        return Utils.get_date_string()[0], TimeChecker.format_hour(TimeChecker.get_hour())
+
     def __fill_meeting_names(self):
         with sqlite3.connect(self.__path) as db:
             data = db.execute("SELECT name FROM meetings").fetchall()
             self.__meeting_names = [i[0] for i in data]
 
-    def insert_meeting_with_validation(self, meeting_name: str):
-        name = "_".join(meeting_name.lower().split(" "))
+    def __insert_meeting_with_validation(self, meeting_name: str):
+        name = self.__format_meeting_name(meeting_name)
         if not name in self.__meeting_names:
             with sqlite3.connect(self.__path) as db:
-                db.execute(f"INSERT INTO meetings (name) VALUES ({name})")
+                db.execute(f'INSERT INTO meetings (name) VALUES ("{name}")')
                 db.commit()
         self.__fill_meeting_names()
 
-    def insert_meeting_join_record(self):
-        pass
+    def insert_meeting_join_record(self, meeting_name: str):
+        self.__insert_meeting_with_validation(meeting_name)
+        name = self.__format_meeting_name(meeting_name)
+        meeting_id = self.__meeting_names.index(name) + 1
 
-    def insert_program_open_record(self):
-        pass
+        date, hour = self.__get_date_and_hour()
+        with sqlite3.connect(self.__path) as db:
+            db.execute("INSERT INTO joins (meeting_id, date_joined, hour_joined) " +
+                       f'VALUES ({meeting_id}, "{date}", "{hour}")')
+            db.commit()
+
+    def insert_program_opened_record(self):
+        date, hour = self.__get_date_and_hour()
+        with sqlite3.connect(self.__path) as db:
+            db.execute("INSERT INTO times_program_opened (date_opened, hour_opened) " +
+                       f'VALUES ("{date}", "{hour}")')
+            db.commit()
