4bac3992677dff3aadd4e85048731dd92fd8b033
DragonWF <plarisanmarc@gmail.com>
2022-05-04 14:04:41 +0800

    Fixed grouped_meeting_joins view

--- sql/create-group-meeting-joins-view.sql
+++ sql/create-group-meeting-joins-view.sql
@@ -1,7 +1,7 @@
 CREATE VIEW grouped_meeting_joins AS
 SELECT m.id AS meeting_id,
        m.name AS meeting_name,
-	   COUNT(mj.id) AS times_meeting_joined
+	   COUNT(m.id) AS times_meeting_joined
 FROM meetings m
-JOIN meeting_joins mj ON mj.id = m.id
-GROUP BY mj.id
+JOIN meeting_joins mj ON mj.meeting_id = m.id
+GROUP BY m.id
